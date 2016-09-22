a53f02de6cbbeac5de3f25ab072ed54df3664025
Bill Maxwell <bill@rancher.com>
2016-09-22 12:27:43 -0700

    Wait for labels to appear in metadata
    Metadata sometimes doesn't have the labels populated. So wait for
    the values to be populated. There should never be a time when labels
    are completely empty since Rancher and K8s both add labels.

--- agent/metadata.go
+++ agent/metadata.go
@@ -1,8 +1,8 @@
 package agent
 
 import (
-	"errors"
 	"strings"
+	"time"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/docker/engine-api/types/events"
@@ -18,7 +18,6 @@ type ContainerEventMessage struct {
 }
 
 func (cem *ContainerEventMessage) SetUUIDFromMetadata(mdCli *metadata.Client) error {
-	var uuid string
 	nameKey := "name"
 	verifyKey := "io.rancher.container.uuid"
 
@@ -34,30 +33,33 @@ func (cem *ContainerEventMessage) SetUUIDFromMetadata(mdCli *metadata.Client) er
 	name = strings.Replace(name, "r-", "", 1)
 	logrus.Debugf("Using: %s as a container name", name)
 
-	containers, err := mdCli.GetContainers()
-	if err != nil {
-		logrus.Errorf("Failed to get containers: %s", err)
-		return err
+	container := metadata.Container{}
+	for len(container.Labels) == 0 {
+		containers, err := mdCli.GetContainers()
+		if err != nil {
+			return err
+		}
+		container = loopContainers(name, verifyKey, cem.Event.Actor.Attributes[verifyKey], containers)
+		time.Sleep(100 * time.Millisecond)
 	}
 
+	logrus.Debugf("UUID: %s found", container.UUID)
+	cem.UUID = container.UUID
+
+	return nil
+}
+
+func loopContainers(name, vKey, expectedVKeyValue string, containers []metadata.Container) metadata.Container {
 	for _, container := range containers {
 		if container.Name == name {
-			if value, ok := container.Labels[verifyKey]; ok {
-				if value == cem.Event.Actor.Attributes[verifyKey] {
-					uuid = container.UUID
-					break
+			if value, ok := container.Labels[vKey]; ok {
+				if value == expectedVKeyValue {
+					return container
 				}
 			}
 		}
 	}
 
-	if uuid == "" {
-		logrus.Debugf("No UUID Found")
-		return errors.New("No UUID found")
-	}
+	return metadata.Container{}
 
-	logrus.Debugf("UUID: %s found", uuid)
-	cem.UUID = uuid
-
-	return nil
 }
